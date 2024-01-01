6c7d82ef5a4b67377ea444ebed6e865a5f79dbf3
Koseemani Ayo <you@example.com>
2024-01-02 00:11:38 +0100

    chore: soft change

--- src/components/icons/ModalCloseIcon.jsx
+++ src/components/icons/ModalCloseIcon.jsx
@@ -11,21 +11,21 @@ function ModalCloseIcon() {
     >
       <path
         d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
-        stroke="#8C8C8C"
+        stroke="#fff"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M9.16992 14.8299L14.8299 9.16992"
-        stroke="#8C8C8C"
+        stroke="#fff"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
       />
       <path
         d="M14.8299 14.8299L9.16992 9.16992"
-        stroke="#8C8C8C"
+        stroke="#fff"
         strokeWidth="1.5"
         strokeLinecap="round"
         strokeLinejoin="round"
